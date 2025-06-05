42105a54fe78727779b6aaee332fdecde6378129
xDepcio <olek.drwal@gmail.com>
2025-06-06 01:38:07 +0200

    databse function return exception err

--- src/olx_scraper/database/database.py
+++ src/olx_scraper/database/database.py
@@ -7,30 +7,33 @@ from psycopg2.pool import AbstractConnectionPool
 
 
 @contextmanager
-def get_connection(pool: AbstractConnectionPool) -> Generator[Result[connection, str]]:
+def get_connection(
+    pool: AbstractConnectionPool,
+) -> Generator[Result[connection, Exception]]:
     conn: connection = pool.getconn()  # type: ignore
     if not isinstance(conn, connection):
-        yield Err("Failed to get a connection from the pool")
+        yield Err(Exception("Failed to get a connection from the pool"))
     else:
         yield Ok(conn)
         pool.putconn(conn)  # type: ignore
 
 
 @contextmanager
-def get_cursor_from_connection(conn: connection) -> Generator[Result[cursor, str]]:
+def get_cursor_from_connection(
+    conn: connection,
+) -> Generator[Result[cursor, Exception]]:
     try:
         cursor = conn.cursor()
         yield Ok(cursor)
         cursor.close()
     except Exception as e:
-        yield Err(f"Error creating cursor: {e}")
+        yield Err(e)
 
 
 @contextmanager
 def get_cursor_from_pool(
-        pool: AbstractConnectionPool,
-        commit: bool = False
-) -> Generator[Result[cursor, str]]:
+    pool: AbstractConnectionPool, commit: bool = False
+) -> Generator[Result[cursor, Exception]]:
     with get_connection(pool) as conn_result:
         match conn_result:
             case Ok(conn):
@@ -38,21 +41,38 @@ def get_cursor_from_pool(
                     yield cursor
                 if commit:
                     conn.commit()
-            case Err(error):
-                yield Err(f"Error getting connection: {error}")
+            case Err() as err:
+                yield err
 
 
 def exec_query(
-        pool: AbstractConnectionPool, query: str, params: list[Any], commit=False
-) -> Result[list[Any], str]:
-    with get_cursor_from_pool(pool, commit=commit) as cursor:
+    pool: AbstractConnectionPool, query: str, params: list[Any]
+) -> Result[list[tuple[Any, ...]], Exception]:
+    with get_cursor_from_pool(pool, commit=True) as cursor:
         match cursor:
             case Ok(c):
                 try:
                     c.execute(query, params)
                     return Ok(c.fetchall())
                 except Exception as e:
-                    return Err(f"Error executing query: {e}")
+                    return Err(e)
+            case Err() as err:
+                return err
+            case _:
+                return Err(f"Unknown cursor object: {cursor}")
+
+
+def exec_many_query(
+    pool: AbstractConnectionPool, query: str, params: list[list[Any]]
+) -> Result[None, Exception]:
+    with get_cursor_from_pool(pool, commit=True) as cursor:
+        match cursor:
+            case Ok(c):
+                try:
+                    c.executemany(query, params)
+                    return Ok(None)
+                except Exception as e:
+                    return Err(e)
             case Err() as err:
                 return err
             case _:
