61710f485b142aadc40cc337aeaff1cd7487fa5e
xDepcio <olek.drwal@gmail.com>
2025-06-06 01:49:01 +0200

    remove explicit commit

--- src/olx_scraper/scrapers/scrape_categories.py
+++ src/olx_scraper/scrapers/scrape_categories.py
@@ -8,17 +8,20 @@ from olx_scraper.result import Result, Err, Ok
 def make_insert_query_from_category(c: Category) -> Result[str, str]:
     query = (
         f"INSERT INTO category (id, type, name, parent_id) VALUES ({c.id}, '{c.type}', '{c.name}', {c.parent if c.parent is not None else "NULL"})"
-        f" ON CONFLICT (id) DO UPDATE SET type = '{c.type}', name = '{c.name}', parent_id = {c.parent if c.parent is not None else "NULL"};")
+        f" ON CONFLICT (id) DO UPDATE SET type = '{c.type}', name = '{c.name}', parent_id = {c.parent if c.parent is not None else "NULL"};"
+    )
     return Ok(query)
 
 
-def exec_multiple_queries(pool: AbstractConnectionPool, queries: list[Result[str, str]]) -> Result[None, str]:
+def exec_multiple_queries(
+    pool: AbstractConnectionPool, queries: list[Result[str, str]]
+) -> Result[None, str]:
     for query in queries:
         match query:
             case Err() as err:
                 return Err(err.error)
             case Ok():
-                exec_query(pool, query.value, [], True)
+                exec_query(pool, query.value, [])
     return Ok(None)
 
 
